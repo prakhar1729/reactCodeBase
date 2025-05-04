870e619630f5e7d3756e6a5e705d9889e047e94d
Prakhar <prakharsingh1729@gmail.com>
2025-05-04 18:46:59 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+const heading = React.createElement(
+  "h1",
+  { id: "headingColor" },
+  "hello prakhar from react"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "childHeading" }, "I am an h1 tag"),
+    React.createElement("h2", { id: "childHeading" }, "I am an h2 tag"),
+  ])
+);
+root.render(parent);
