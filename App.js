bd75466527c353d6630979482de6d2a2c6a6151f
Prakhar <prakharsingh1729@gmail.com>
2025-08-14 17:10:35 +0530

    new changes

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
-const heading = React.createElement(
-  "h1",
-  { id: "headingColor" },
-  "hello prakhar from react"
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "childHeading" }, "I am an h1 tag"),
-    React.createElement("h2", { id: "childHeading" }, "I am an h2 tag"),
+    React.createElement(
+      "h1",
+      { id: "childHeading", key: "childHeading" },
+      "I am an h1 tag"
+    ),
+    React.createElement("h2", { key: "childHeading2" }, "I am an h2 tag"),
   ])
 );
+
+const root = createRoot(document.getElementById("root"));
 root.render(parent);
